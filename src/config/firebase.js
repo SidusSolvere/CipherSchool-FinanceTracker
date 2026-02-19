67e10a33ba3e23b421b8e882e1d398ea2476174b
SidusSolvere <135128689+SidusSolvere@users.noreply.github.com>
2026-02-19 15:26:49 +0530

    env changes

--- src/config/firebase.js
+++ src/config/firebase.js
@@ -10,9 +10,9 @@ import {getFirestore} from "firebase/firestore";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyAr0kp3kL6UFhOagwcpxIXyA22LoWG35h8",
-  authDomain: "financetracker-8bcc4.firebaseapp.com",
-  projectId: "financetracker-8bcc4",
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
   storageBucket: "financetracker-8bcc4.firebasestorage.app",
   messagingSenderId: "580154214846",
   appId: "1:580154214846:web:6ff1253f4dc7520af191ef",

891b4c64269de103c4bf58cceb3242859d776105
SidusSolvere <135128689+SidusSolvere@users.noreply.github.com>
2026-02-19 05:42:21 +0530

    added data insert operation

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -85,7 +85,7 @@ define(['./workbox-7a5e81cd'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.i1ec49fdcl"
+    "revision": "0.jh5s6ami3jg"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
--- src/components/table.jsx
+++ src/components/table.jsx
@@ -1,12 +1,15 @@
 import { useState } from "react";
-import { db } from "../config/firebase";
-import { getDocs, collection } from "firebase/firestore";
+import { db, auth } from "../config/firebase";
+import { getDocs, collection, addDoc } from "firebase/firestore";
 
 function Table() {
   const [transactions, setTransactions] = useState([]);
-
-  // ⚠️ use correct collection name
   const transactionRef = collection(db, "Transactions");
+  const [amount, setAmount] = useState(0);
+  const [date, setDate] = useState("");
+  const [toFrom, setToFrom] = useState("");
+  const [transactionId, setTransactionId] = useState("");
+  const [type, setType] = useState("");
 
   const getTransactions = async () => {
     try {
@@ -16,9 +19,23 @@ function Table() {
         id: doc.id,
         ...doc.data(),
       }));
-      console.log({filteredData});
+      console.log({ filteredData });
 
       setTransactions(filteredData);
+    } catch (err) {
+      console.error(err);
+    }
+  };
+  const onSubmit = async () => {
+    try {
+      await addDoc(transactionRef, {
+        Amount: amount,
+        Date: date,
+        ToFrom: toFrom,
+        TransactionId: transactionId,
+        Type: type,
+        UserId: auth.currentUser.uid,
+      });
     } catch (err) {
       console.error(err);
     }
@@ -27,16 +44,49 @@ function Table() {
   return (
     <>
       <button onClick={getTransactions}>Get Transactions</button>
+      <input
+        type="number"
+        placeholder="Amount"
+        value={amount}
+        onChange={(e) => setAmount(e.target.value)}
+      />
 
+      <input
+        type="datetime-local"
+        value={date}
+        onChange={(e) => setDate(e.target.value)}
+      />
+
+      <input
+        type="text"
+        placeholder="To / From"
+        value={toFrom}
+        onChange={(e) => setToFrom(e.target.value)}
+      />
+
+      <input
+        type="text"
+        placeholder="Transaction ID"
+        value={transactionId}
+        onChange={(e) => setTransactionId(e.target.value)}
+      />
+
+      <select value={type} onChange={(e) => setType(e.target.value)}>
+        <option value="">Select type</option>
+        <option value="Debit">Debit</option>
+        <option value="Credit">Credit</option>
+      </select>
+
+      <button onClick={onSubmit}>Add Transaction</button>
       <div>
         {transactions.map((tx) => (
-  <div key={tx.id}>
-    <p>
-      ₹{tx.Amount} | {tx.Type} | {tx.ToFrom}  | {tx.TransactionId} | {tx.Date.toDate().toLocaleString()}
-    </p>
-  </div>
-))}
-
+          <div key={tx.id}>
+            <p>
+              ₹{tx.Amount} | {tx.Type} | {tx.ToFrom} | {tx.TransactionId} |{" "}
+              {tx.Date.toDate().toLocaleString()}
+            </p>
+          </div>
+        ))}
       </div>
     </>
   );
